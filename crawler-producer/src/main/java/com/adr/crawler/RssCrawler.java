56a40dd50d942faccda9ae54d49c43e8720d01aa
sharoha <sharmarohan136@gmail.com>
2023-11-01 22:05:17 +0530

    Intial project setup with multimodule build running

---
+++ crawler-producer/src/main/java/com/adr/crawler/RssCrawler.java
@@ -0,0 +1,11 @@
+package com.adr.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssCrawler {
+    public static void main(String[] args) {
+        SpringApplication.run(RssCrawler.class);
+    }
+}
---
+++ crawler-producer/src/main/java/com/adr/producer/RssDataProducer.java
@@ -0,0 +1,4 @@
+package com.adr.producer;
+
+public class RssDataProducer {
+}
